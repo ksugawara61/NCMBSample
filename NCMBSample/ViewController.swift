25ac4bf249b985e15af4c3a058cba417f516e269
ksugawara61 <katsuya@kokohadoko.net>
2017-09-05 01:17:05 +0900

    first commit

--- NCMBSample/ViewController.swift
+++ NCMBSample/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import NCMB
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var sampleTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,76 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func save() {
+        let object = NCMBObject(className: "Message")
+        object?.setObject(sampleTextField.text, forKey: "text")
+        object?.saveInBackground({ (error) in
+            if error != nil {
+                // エラー発生時
+                print("*error*")
+            } else {
+                // 保存成功時
+                print("success")
+            }
+        })
+    }
+    
+    @IBAction func read() {
+        let query = NCMBQuery(className: "Message")
+        query?.findObjectsInBackground({ (result, error) in
+            if error != nil {
+                // エラー発生時
+                print("*error*")
+            } else {
+                // データ取得時
+                let messages = result as! [NCMBObject]
+                let text = messages.last?.object(forKey: "text") as! String
+                
+                self.sampleTextField.text = text
+            }
+        })
+    }
+    
+    @IBAction func update() {
+        let query = NCMBQuery(className: "Message")
+        query?.whereKey("text", equalTo: "こんばんは")
+        query?.findObjectsInBackground({ (result, error) in
+            if error != nil {
+                print("*error")
+            } else {
+                let messages = result as! [NCMBObject]
+                let textObject = messages.first
+                textObject?.setObject("おやすみ", forKey: "text")
+                textObject?.saveInBackground({ (error) in
+                    if error != nil {
+                        print("*error*")
+                    } else {
+                        print("update success")
+                    }
+                })
+            }
+        })
+    }
+    
+    @IBAction func delete() {
+        let query = NCMBQuery(className: "Message")
+        query?.whereKey("text", equalTo: "test")
+        query?.findObjectsInBackground({ (result, error) in
+            if error != nil {
+                print("*error*")
+            } else {
+                let messages = result as! [NCMBObject]
+                let textObject = messages.first
+                textObject?.deleteInBackground({ (error) in
+                    if error != nil {
+                        print("*error*")
+                    } else {
+                        print("delete success")
+                    }
+                })
+            }
+        })
+    }
 
 }
 
